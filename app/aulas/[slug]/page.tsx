0ff4798f82da73cba5880e8f116d37308515fbd4
Samuel Henrique Tomaselli <samueltomaselli2005@gmail.com>
2024-05-20 13:56:20 -0300

    add: dark theme

--- app/aulas/[slug]/page.tsx
+++ app/aulas/[slug]/page.tsx
@@ -13,7 +13,7 @@ export default async function BlogPage({ params }: { params: { slug: string } })
   return (
     <main className="md:px-16 md:py-10 px-10 py-5 ">
 
-      <article className="flex flex-col prose max-w-4xl prose-sm  prose-h1:font-bold prose-h1:text-2xl prose-img:max-w-96 prose-img:shadow-md">
+      <article className="flex flex-col prose max-w-4xl prose-sm prose-h1:font-bold prose-h1:text-2xl prose-img:max-w-96 prose-img:shadow-md prose-h1:dark:text-white prose-h2:dark:text-gray-200 prose-h3:dark:text-gray-200 prose-p:dark:text-[#7F8EA3] prose-ul:dark:text-[#7F8EA3] ">
         {blog.content}
       </article>
     </main>
--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,8 @@ import Nav from "@/components/sections/nav";
 import SearchInput from "@/components/sections/search-input";
 import { getBlogs } from "@/app/aulas/fetchers";
 import MobileNav from "@/components/sections/mobile-nav";
+import { ThemeProvider } from "@/components/sections/theme-provider";
+import { ModeToggle } from "@/components/sections/toggle";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -33,38 +35,47 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr] ">
-          <div className="hidden border-r bg-muted/50 md:block">
-            <div className="flex h-full max-h-screen flex-col gap-2">
-              <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-                <Link href="/" className="flex items-center gap-2 font-semibold">
-                  <GraduationCap className="h-6 w-6" />
-                  <span className="">Projeto</span>
-                </Link>
-              </div>
-              <div className="flex-1">
-                <Nav blogs={blogs} />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr] ">
+            <div className="hidden border-r bg-muted/50 md:block">
+              <div className="flex h-full max-h-screen flex-col gap-2">
+                <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+                  <Link href="/" className="flex items-center gap-2 font-semibold">
+                    <GraduationCap className="h-6 w-6" />
+                    <span className="">Projeto</span>
+                  </Link>
+
+                </div>
+                <div className="flex-1">
+                  <Nav blogs={blogs} />
+                </div>
               </div>
             </div>
+            <div className="flex flex-col">
+              <header className="flex h-14 items-center gap-4 border-b bg-muted/50 px-4 lg:h-[60px] lg:px-6">
+                <Sheet>
+                  <SheetTrigger asChild>
+                    <Button variant="outline" size="icon" className="shrink-0 md:hidden">
+                      <Menu className="h-5 w-5" />
+                      <span className="sr-only">Toggle navigation menu</span>
+                    </Button>
+                  </SheetTrigger>
+                  <SheetContent side="left" className="flex flex-col">
+                    <MobileNav blogs={blogs} />
+                  </SheetContent>
+                </Sheet>
+                <SearchInput />
+                <ModeToggle />
+              </header>
+              {children}
+            </div>
           </div>
-          <div className="flex flex-col">
-            <header className="flex h-14 items-center gap-4 border-b bg-muted/50 px-4 lg:h-[60px] lg:px-6">
-              <Sheet>
-                <SheetTrigger asChild>
-                  <Button variant="outline" size="icon" className="shrink-0 md:hidden">
-                    <Menu className="h-5 w-5" />
-                    <span className="sr-only">Toggle navigation menu</span>
-                  </Button>
-                </SheetTrigger>
-                <SheetContent side="left" className="flex flex-col">
-                  <MobileNav blogs={blogs} />
-                </SheetContent>
-              </Sheet>
-              <SearchInput />
-            </header>
-            {children}
-          </div>
-        </div>
+        </ThemeProvider>
       </body>
     </html>
   );
