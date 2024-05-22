31035394db897a6785eb81e98cadd514172ff3a9
Samuel Henrique Tomaselli <samueltomaselli2005@gmail.com>
2024-05-22 00:17:31 -0300

    add: highlight code

--- app/aulas/[slug]/page.tsx
+++ app/aulas/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { getBlogBySlug, getAllBlogSlug, getBlogs } from "../fetchers";
-import Variaveis from "@/app/aulas/_mdx-content/variaveis.mdx";
+import "@/styles/highlightJs/github-dark.css";
 
 export async function generateStaticParams() {
   return getAllBlogSlug();
@@ -12,8 +12,7 @@ export default async function BlogPage({ params }: { params: { slug: string } })
 
   return (
     <main className="md:px-16 md:py-10 px-10 py-5 ">
-
-      <article className="flex flex-col prose max-w-4xl prose-sm prose-h1:font-bold prose-h1:text-2xl prose-img:max-w-96  prose-img:opacity-70 dark:prose-invert prose-pre:dark:bg-[#1f2937]">
+      <article className="flex flex-col prose prose-code:text-xs max-w-4xl prose-sm prose-h1:font-bold prose-h1:text-2xl prose-img:max-w-96 dark:prose-invert prose-pre:dark:bg-[#1f2937]">
         {blog.content}
       </article>
     </main>
