32b321139ee1d91d15c52043f32a748dfbf23a8e
Samuel Henrique Tomaselli <samueltomaselli2005@gmail.com>
2024-05-22 11:45:58 -0300

    minor

--- app/calendario/page.tsx
+++ app/calendario/page.tsx
@@ -19,13 +19,13 @@ export default function Dashboard() {
                     selected={selectedDates}
                     className="w-full h-full" />
                 <section className="mt-12">
-                    <h2 className="text-base font-semibold leading-6 text-gray-900">Próximas aulas</h2>
-                    <ol className="mt-2 divide-y divide-gray-200 text-sm leading-6 text-gray-500">
+                    <h2 className="text-base font-semibold leading-6 ">Próximas aulas</h2>
+                    <ol className="mt-2 divide-y text-sm leading-6 ">
                         <li className="py-4 sm:flex">
                             <time dateTime="2022-01-19" className="w-28 flex-none">
                                 Sábado, Jun 8
                             </time>
-                            <p className="mt-2 flex-auto font-semibold text-gray-900 sm:mt-0">Aula 01</p>
+                            <p className="mt-2 flex-auto font-semibold  sm:mt-0">Aula 01</p>
                             <p className="flex-none sm:ml-6">
                                 <time dateTime="2022-01-13T14:30">07:00 AM</time> - <time dateTime="2022-01-13T16:30">10:15 AM</time>
                             </p>
@@ -34,7 +34,7 @@ export default function Dashboard() {
                             <time dateTime="2022-01-18" className="w-28 flex-none">
                                 Sábado, Jun 15
                             </time>
-                            <p className="mt-2 flex-auto font-semibold text-gray-900 sm:mt-0">Aula 02</p>
+                            <p className="mt-2 flex-auto font-semibold  sm:mt-0">Aula 02</p>
                             <p className="flex-none sm:ml-6">
                                 <time dateTime="2022-01-17T10:00">07:00 AM</time> - <time dateTime="2022-01-17T10:15">10:15 AM</time>
                             </p>
@@ -43,7 +43,7 @@ export default function Dashboard() {
                             <time dateTime="2022-01-18" className="w-28 flex-none">
                                 Sábado, Jun 22
                             </time>
-                            <p className="mt-2 flex-auto font-semibold text-gray-900 sm:mt-0">Aula 03</p>
+                            <p className="mt-2 flex-auto font-semibold  sm:mt-0">Aula 03</p>
                             <p className="flex-none sm:ml-6">
                                 <time dateTime="2022-01-17T10:00">07:00 AM</time> - <time dateTime="2022-01-17T10:15">10:15 AM</time>
                             </p>
@@ -52,7 +52,7 @@ export default function Dashboard() {
                             <time dateTime="2022-01-18" className="w-28 flex-none">
                                 Sábado, Jun 29
                             </time>
-                            <p className="mt-2 flex-auto font-semibold text-gray-900 sm:mt-0">Aula 04</p>
+                            <p className="mt-2 flex-auto font-semibold  sm:mt-0">Aula 04</p>
                             <p className="flex-none sm:ml-6">
                                 <time dateTime="2022-01-17T10:00">07:00 AM</time> - <time dateTime="2022-01-17T10:15">10:15 AM</time>
                             </p>
