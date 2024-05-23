e7f4bd243644be9f8a83ddbbeefad21c6dde5bfc
Samuel Henrique Tomaselli <samueltomaselli2005@gmail.com>
2024-05-23 14:12:04 -0300

    fixes

--- components/sections/mobile-nav.tsx
+++ components/sections/mobile-nav.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { GraduationCap, Home, Library, Mail } from "lucide-react";
+import { Calendar, GraduationCap, Home, Library, Mail } from "lucide-react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React, { useState } from "react";
@@ -23,18 +23,16 @@ function MobileNav({ blogs }: Props) {
       <Link
         onClick={() => setActiveItem("Dashboard")}
         href="/"
-        className={`flex items-center gap-3 rounded-lg px-3 py-2 ${
-          activeItem === "Dashboard" ? "bg-muted" : ""
-        } transition-all hover:text-primary`}
+        className={`flex items-center gap-3 rounded-lg px-3 py-2 ${activeItem === "Dashboard" ? "bg-muted" : ""
+          } transition-all hover:text-primary`}
       >
         <Home className="h-4 w-4" />
         Home
       </Link>
       <div className={`flex-col gap-3 rounded-lg cursor-pointer transition-all`}>
         <div
-          className={`${
-            activeItem === "Aulas" ? "bg-muted" : ""
-          } flex items-center gap-3 px-3 py-2 hover:text-primary rounded-lg transition-all`}
+          className={`${activeItem === "Aulas" ? "bg-muted" : ""
+            } flex items-center gap-3 px-3 py-2 hover:text-primary rounded-lg transition-all`}
           onClick={() => {
             setActiveItem("Aulas");
             setCollapse(!collapse);
@@ -60,16 +58,15 @@ function MobileNav({ blogs }: Props) {
                   .toLowerCase()
                   .replace(/\s+/g, "-")}`}
                 key={i}
-                className={`ml-2 py-2 px-2 text-sm hover:text-primary ${
-                  activeNavItem ===
+                className={`ml-2 py-2 px-2 text-sm hover:text-primary ${activeNavItem ===
                   item.frontmatter.title
                     .normalize("NFD")
                     .replace(/[\u0300-\u036f]/g, "")
                     .toLowerCase()
                     .replace(/\s+/g, "-")
-                    ? "bg-muted/80 text-primary rounded-lg"
-                    : ""
-                }`}
+                  ? "bg-muted/80 text-primary rounded-lg"
+                  : ""
+                  }`}
               >
                 {item.frontmatter.title}
               </Link>
@@ -79,11 +76,20 @@ function MobileNav({ blogs }: Props) {
       </div>
 
       <Link
+        onClick={() => setActiveItem("Calendario")}
+        href="/calendario"
+        className={`flex items-center gap-3 rounded-lg px-3 py-2 ${activeItem === "Contato" ? "bg-muted" : ""
+          } transition-all hover:text-primary`}
+      >
+        <Calendar className="h-4 w-4" />
+        Calendário
+      </Link>
+
+      <Link
         onClick={() => setActiveItem("Contato")}
         href="/contato"
-        className={`flex items-center gap-3 rounded-lg px-3 py-2 ${
-          activeItem === "Contato" ? "bg-muted" : ""
-        } transition-all hover:text-primary`}
+        className={`flex items-center gap-3 rounded-lg px-3 py-2 ${activeItem === "Contato" ? "bg-muted" : ""
+          } transition-all hover:text-primary`}
       >
         <Mail className="h-4 w-4" />
         Contato
